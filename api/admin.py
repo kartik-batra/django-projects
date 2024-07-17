c73fd8273be1c05ce05f9ffecb92b11fdb9d7279
kartik-batra <147294693+kartik-batra@users.noreply.github.com>
2024-07-17 13:35:48 +0530

    Add files via upload

---
+++ api/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import SocialAccount, UserPost, Campaign, Schedule
+# Register your models here.
+@admin.register(SocialAccount)
+class AdminSocialAccount(admin.ModelAdmin):
+    list_display = ['user_id', 'account_type', 'access_token']
+
+@admin.register(UserPost)
+class AdminUserPost(admin.ModelAdmin):
+    list_display = ['user_id',SocialAccount, 'description', 'attachments', 'insta_desc', 'insta_story', 'insta_first_comment',
+                    'linkedin_desc', 'pinterest_desc', 'twitter_desc', 'youtube_desc', 'isDraft', 'isScheduled',Schedule, Campaign]
+
+@admin.register(Campaign)
+class AdminCampaign(admin.ModelAdmin):
+    list_display = ['post_id', 'name', 'date', 'description', 'status']
+
+@admin.register(Schedule)
+class AdminSchedule(admin.ModelAdmin):
+    list_display = ['post_id', 'date_time', 'time_zone']
+
+
+    
---
+++ api/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+import pytz
+
+TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+# Create your models here.
+social_platforms = [['Facebook' , 'Facebook' ],
+                    ['Instagram', 'Instagram'],
+                    ['Twitter' , 'Twitter' ],
+                    ['Pinterest' , 'Pinterest' ],
+                    ['LinkedIn' , 'LinkedIn' ],
+                    ['Youtube', 'Youtube'],
+                    ]
+class SocialAccount(models.Model):
+    user_id = models.CharField(max_length=100,primary_key=True,unique=True, blank=False)
+    account_type = models.CharField(max_length=500,unique=True,choices=social_platforms, blank=False)
+    access_token = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.account_type
+    
+class Schedule(models.Model):
+    post_id = models.CharField(primary_key=True,max_length=50)
+    date_time = models.DateTimeField(unique=True)
+    time_zone = models.CharField(max_length=32, choices=TIMEZONES, default='UTC')
+
+    def __str__(self):
+        return self.post_id
+
+STATUS_CHOICES = (('Active', 'Active'),
+                  ('Inactive','Inactive'),
+                  ('Finished','Finished'),)
+
+class Campaign(models.Model):
+    post_id = models.CharField(primary_key=True,max_length=50)
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.CharField(max_length=500)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.name
+    
+class UserPost(models.Model):
+    user_id = models.CharField(max_length=100,primary_key=True,unique=True, blank=False)
+    selected_accounts = models.ManyToManyField(SocialAccount)
+    description = models.TextField(max_length=500,)
+    attachments = models.FileField()
+    insta_desc = models.TextField(max_length=500,)
+    insta_story = models.BooleanField(default=False)
+    insta_first_comment = models.BooleanField(default=False)
+    linkedin_desc = models.TextField(max_length=500,)
+    pinterest_desc = models.TextField(max_length=500,)
+    twitter_desc = models.TextField(max_length=500,)
+    youtube_desc = models.TextField(max_length=500,)
+    isDraft = models.BooleanField(default=False)
+    isScheduled = models.BooleanField(default=False)
+
+    schedule = models.ManyToManyField(Schedule)
+    campaign = models.ManyToManyField(Campaign)
+
