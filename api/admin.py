db2805c0663900dd480eff2c50cf75750c26479d
kartik-batra <147294693+kartik-batra@users.noreply.github.com>
2024-07-23 14:08:28 +0530

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -7,16 +7,13 @@ class AdminSocialAccount(admin.ModelAdmin):
 
 @admin.register(UserPost)
 class AdminUserPost(admin.ModelAdmin):
-    list_display = ['user_id',SocialAccount, 'description', 'attachments', 'insta_desc', 'insta_story', 'insta_first_comment',
-                    'linkedin_desc', 'pinterest_desc', 'twitter_desc', 'youtube_desc', 'isDraft', 'isScheduled',Schedule, Campaign]
+    list_display = ['id','user_id',SocialAccount, 'description', 'attachments', 'insta_desc', 'insta_story', 'insta_first_comment',
+                    'linkedin_desc', 'pinterest_desc', 'twitter_desc', 'youtube_desc', 'isDraft', 'isScheduled',Schedule, 'campaign']
 
 @admin.register(Campaign)
 class AdminCampaign(admin.ModelAdmin):
-    list_display = ['post_id', 'name', 'date', 'description', 'status']
+    list_display = ['campaign_id', 'name', 'date', 'description', 'status']
 
 @admin.register(Schedule)
 class AdminSchedule(admin.ModelAdmin):
-    list_display = ['post_id', 'date_time', 'time_zone']
-
-
-    
+    list_display = ['schedule_id', 'date_time', 'time_zone']
